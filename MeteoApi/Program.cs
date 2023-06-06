9e65abbf505afbf9c9a3a97948f4d861e4760396
Yassine Ben Atitallah <yassine.benatitallah-ext@lupindev.cloud>
2023-06-06 11:40:48 +0200

    get api

--- MeteoApi/Program.cs
+++ MeteoApi/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +12,28 @@ namespace MeteoApi
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("hello in api meteo");
+            Console.ReadLine();
+            CallWebAPIAsync().Wait();
+        }
+        static async Task CallWebAPIAsync()
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://data.mobilites-m.fr/api/routers/default/index/routes");
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                HttpResponseMessage response = await client.GetAsync("?reseaux=TRAM&codes=SEM_A");
+                if (response.IsSuccessStatusCode)
+                {
+                    Tram tram = await response.Content.ReadAsAsync<Tram>();
+                    Console.WriteLine("Id:{0}\tName:{1}", tram.LongName, tram.ShortName);
+                }
+                else
+                {
+                    Console.WriteLine("Internal server Error");
+                }
+            }
         }
     }
 }
---
+++ MeteoApi/Tram.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MeteoApi
+{
+    public class Tram
+    {
+        public string ShortName
+        { get; set; }
+
+        public string LongName
+        { get; set; }
+    }
+}
